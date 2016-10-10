245d0323c5837922767251cf22887d5fc3771178
Ehsan Ashouri <ehsan.ashouri@net-a-porter.com>
2016-10-10 16:09:46 +0100

    NOJIRA: digital ocean script to build droplet

---
+++ digitalocean.sh
@@ -0,0 +1,24 @@
+PS3='Please enter your choice: '
+options=("list droplets" "nyc droplet" "destroy droplet" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "list droplets")
+            echo "you chose choice 1"
+            doctl compute droplet list
+            ;;
+        "nyc droplet")
+            echo "you chose choice 2"
+            doctl compute droplet create test --size 1gb --image debian-8-x64 --region nyc1 --ssh-keys $INSERT_KEY_FROM_CONSOLE --enable-backups
+            ;;
+        "destroy droplet")
+            echo "you chose choice 3"
+	    read -p "enter the droplet id you want to destroy: " dropletid
+            doctl compute droplet delete $dropletid
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
